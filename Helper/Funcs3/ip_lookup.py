8140c2dd7b1f08ada3582ab4c9433dda36104a9e
Vatos <92579954+VatosV2@users.noreply.github.com>
2024-03-21 14:14:37 +0100

    Add files via upload

---
+++ Helper/Funcs3/ip_lookup.py
@@ -0,0 +1,13 @@
+from Helper import *
+from Helper.Common.utils import *
+
+def ip_lookup():
+    ip = input(f"{Fore.RESET}[{Fore.LIGHTMAGENTA_EX}>{Fore.RESET}] IP Address: ")
+    response = requests.get(f"http://ip-api.com/json/{ip}")
+    data = response.json()
+    print(f"{lc} Ip: {ip}")
+    print(f"{lc} Country: " + data['country'])
+    print(f"{lc} City: " + data['city'])
+    print(f"{lc} Region: " + data['regionName'])
+    print(f"{lc} ISP: " + data['isp'])
+    input("Press Enter To continue...")
