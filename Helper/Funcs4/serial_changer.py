7dce363a0a2e6357f02643c698793a30e66bf028
z <149972831+alluding@users.noreply.github.com>
2024-07-12 09:20:45 -0400

    Delete Helper directory

--- Helper/Funcs4/serial_changer.py
+++
@@ -1,9 +0,0 @@
-from Helper import *
-from Helper.Common.utils import *
-
-def serial_changer():
-    print(f"{lc} Changning Serials..")
-    new_serial = ''.join(random.choices(string.digits, k=16))
-    os.system(f"Helper\Plugs\SerialPlugs\AMIDEWINx64.exe /bs {new_serial}")
-    input(f"{lc} Done!")
-    input("Press Enter To continue...")
